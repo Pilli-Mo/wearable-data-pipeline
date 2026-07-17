0b7d2722604cb837ae8c67502d65f95c3f4e8e2c
Pilli-Mo <piramo97@gmail.com>
2026-07-17 21:00:19 +0200

    load daily activity into sqlite database

---
+++ load_data.py
@@ -0,0 +1,14 @@
+import sqlite3 
+import pandas as pd  #  reads CSV files and converts them to DataFrames
+
+# STep 1: connect to a database file 
+conn = sqlite3.connect("wearable_data.db")  # create a database file if it doesn't exist
+
+# Step 2: read the CSV file into a pandas DataFrame
+df = pd.read_csv("data/raw/dailyActivity_merged.csv")
+
+# Step 3: write the DataFrame into a database table
+df.to_sql("dailyActivity", conn, if_exists="replace", index=False)
+
+# Step 4: close the database connection
+conn.close()
